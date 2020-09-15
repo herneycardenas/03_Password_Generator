5916ff99e04f85cb70a503074979922253cec476
Herney Cardenas <i.herneyc@gmail.com>
2020-09-15 11:24:34 -0700

    prompt questions added

--- script.js
+++ script.js
@@ -3,12 +3,35 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+
+  let confirmCharacters = prompt ("How many characters does your password need to be?");
+
+if (confirmCharacters >= 8);
+
+else if (confirmCharacters <=7) {
+ alert("Character length must be more than 8!");
+}
+else if (confirmCharacters >=128) {
+ alert("Character Length Must Be 8-128!");
+}
+
+//let confirmLowercase = confirm("Would you like lowercases characters?");
+  //if (confirmLowercase === true) 
+
+
+let confirmUppercase = confirm ("Would you like uppercases characters?");
+let confirmNumeric = confirm ("Would you like numeric characters?");
+let confirmSpecialCharacters = confirm ("Would you like special characters?");
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+ 
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
